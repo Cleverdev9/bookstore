fddb92ffb0f666a83b1716b3f4e4f9986c9c3dab
Cleverson Coelho <87801153+Cleverdev9@users.noreply.github.com>
2024-02-26 21:12:52 -0300

    proprierties, Book, ORM

---
+++ src/main/java/com/crlsistemas/bookstore/entities/Book.java
@@ -0,0 +1,94 @@
+package com.crlsistemas.bookstore.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_book")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private LocalDate publicationYear;
+	private String description;
+	
+	public Book() {
+		
+	}
+
+	public Book(Long id, String name, Double price, LocalDate publicationYear, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.publicationYear = publicationYear;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public LocalDate getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(LocalDate publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
